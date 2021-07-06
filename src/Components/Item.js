4907eafa998658885e0dd3798c723e6a5b9725fe
natcassidy <nat.cassidy865@gmail.com>
2021-07-05 22:04:11 -0300

    Added ability to click entire ticket box

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,8 +1,8 @@
 import '../Styles/Item.css';
 
-const Item = ({title, priority, onClickListItem, id}) => {
+const Item = ({title, priority, onClickListItem, id, onClickGoBack}) => {
     return (
-        <div className="item">
+        <div className="item" onClick={() => onClickListItem(id)}>
             <p>{title}</p>
             <p>{priority}</p>
             <button onClick={() => onClickListItem(id)}>Click Me</button>
